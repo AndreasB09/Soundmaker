abb3b7bd6440922a670d51e77af85405baea4658
andreasb09 <andreasb09.kodehode@gmail.com>
2024-01-10 13:27:03 +0100

    renamed soundfiles

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 // array with sound files and shortcuts
 
+const soundFolder = "sounds"
+const sounds = [
+  {
+    fileName: "Guitar_Chords1.waw",
+    hotKey: "q",
+  },
+  {
+    fileName: "Guitar_Harmonics1.waw",
+    hotKey: "w",
+  },
+  {
+    fileName: "Guitar_Harmonics2.waw",
+    hotKey: "e",
+  },
+  {
+    fileName: "Sunshine_Bass.waw",
+    hotKey: "r",
+  },
+  {
+    fileName: "Sunshine_Chords.waw",
+    hotKey: "t",
+  },
+  {
+    fileName: "Sunshine_Lead.waw",
+    hotKey: "y",
+  }
+];
+
+const magicSound  = document.getElementById("magic-sound")
